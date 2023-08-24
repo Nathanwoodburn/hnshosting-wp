2209e03158461c6ff2cc27da4e59cc1eea9c13f3
Nathan Woodburn <github@nathan.woodburn.au>
2023-08-24 17:11:42 +1000

    main: Email test 3

--- master/main.py
+++ master/main.py
@@ -275,20 +275,11 @@ def stripeapi():
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(host, port, context=context) as server:
             server.login(user, password)
-            message = """From: Hosting <""" + from_email + """>
-            To: """ + email + """
-            Subject: Your Licence key
-
-            Hello,
-            This email contains your licence key for your new wordpress site.
-            You can redeem this key via the discord bot or api.
-
-            Your licence key is: """ + licence_key + """
-
-            Thanks,
-            HNSHosting
-
-            """
+            message = "From: Hosting <" + from_email + ">\nTo: " + email + \
+                "\nSubject: Your Licence key\n\nHello,\n\n"\
+                +"This email contains your licence key for your new wordpress site.\n" \
+                +"You can redeem this key via the discord bot or api.\n\n"\
+                +"Your licence key is: " + licence_key +"\nThanks,\nHNSHosting"
 
             server.sendmail(from_email, email, message)
 
