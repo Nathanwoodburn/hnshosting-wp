ce8827ed97a334c99974a8e379a2c870db068b0f
Nathan Woodburn <github@nathan.woodburn.au>
2023-08-25 12:49:29 +1000

    worker: Install missing update repo

--- worker/install.sh
+++ worker/install.sh
@@ -11,6 +11,7 @@ KERNEL_VERSION=$(uname -r)
 sudo apt-mark hold linux-image-generic linux-headers-generic linux-generic linux-image-$KERNEL_VERSION linux-headers-$KERNEL_VERSION
 
 # Install Docker
+sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common python3-pip nginx -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
