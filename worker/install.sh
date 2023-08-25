8e9055dcd31f8d0e8c099164e60b208ea2c14c1a
Nathan Woodburn <github@nathan.woodburn.au>
2023-08-25 11:35:04 +1000

    worker: Added pulling docker images to install

--- worker/install.sh
+++ worker/install.sh
@@ -6,19 +6,19 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common python3-pip nginx -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo apt install docker-compose -y
-
-# Install NGINX
-sudo apt install nginx -y
+sudo apt install docker-ce docker-compose -y
 
 # Install python prerequisites
-sudo apt install python3-pip -y
 python3 -m pip install -r requirements.txt
 cp .env.example .env
 chmod +x wp.sh tlsa.sh
+
+# Pull docker images to save time later
+docker pull mysql:5.7 &
+docker pull wordpress:latest &
+wait
